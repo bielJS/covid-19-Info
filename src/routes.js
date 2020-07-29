c59ceddabaa8e26e6c044157b7b042e4c413f497
bielJS <gabriel.ferreira@email.com>
2020-07-29 12:35:30 -0300

    fix rotas inexistentes

--- src/routes.js
+++ src/routes.js
@@ -5,12 +5,12 @@ import Home from './pages/Home';
 import Country from './pages/Country';
 import States from './pages/States';
 
-
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Home} />
+        <Route component={Home} />
         <Route path="/country" component={Country} />
         <Route path="/states" component={States} />
       </Switch>
